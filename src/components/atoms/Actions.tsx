73f3240c3120cc0471d955340e88535f6b822fb4
Rafael Moura <rafael.moura22@fatec.sp.gov.br>
2022-03-09 10:55:10 -0300

    adicionando navegação no componente Actions

--- src/components/atoms/Actions.tsx
+++ src/components/atoms/Actions.tsx
@@ -1,24 +1,33 @@
 import React from 'react';
 import { ScrollView, StyleSheet } from 'react-native';
 import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 
 import { Colors } from '../../config/styles';
 import { ActionItem } from './ActionItem';
+import { Screens } from '../../enums/Screens';
 
 const ACTIONS = [
   {
     name: 'Adicionar\nbolso',
     img: <Feather name="plus-circle" size={36} color={Colors.text} />,
+    route: Screens.NEW_POCKET,
   }
 ]
 
 export const Actions: React.FC = () => {
+  const navigation = useNavigation();
+
+  function handlePress(route: Screens) {
+    navigation.navigate(route);
+  }
+
   return (
     <ScrollView contentContainerStyle={styles.actions} horizontal showsHorizontalScrollIndicator={false}>
       {ACTIONS.map((action, index) => (
         <ActionItem
           key={index}
-          onPress={() => { }}
+          onPress={() => handlePress(action.route)}
           {...action}
         />
       ))}
