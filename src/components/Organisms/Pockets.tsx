83e6652e4fc607c4213afaa13a670de29a8abadd
Rafael Moura <rafael.moura22@fatec.sp.gov.br>
2022-03-09 10:51:16 -0300

    componete pockets

--- src/components/Organisms/Pockets.tsx
+++ src/components/Organisms/Pockets.tsx
@@ -1,2 +1,16 @@
 import React from 'react';
+import { View } from 'react-native';
+import { useAppContext } from '../../Contexts/AppContext';
+import { PocketItem } from '../molecules/PocketItem';
 
+export const Pockets = () => {
+  const { pockets } = useAppContext();
+
+  return (
+    <View>
+      {pockets.map((pocket, index) => (
+        <PocketItem {...pocket} key={index} />
+      ))}
+    </View>
+  );
+}
