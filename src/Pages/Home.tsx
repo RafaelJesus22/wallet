d9087042abc3fd8d9bcce4835a8228e619a3816b
Rafael Moura <rafael.moura22@fatec.sp.gov.br>
2022-03-09 10:54:35 -0300

    refatorando tela Home

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,31 +1,25 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
 import { View, StyleSheet, StatusBar } from 'react-native';
-import { PocketAmount } from '../components/atoms/PocketAmount';
-import { PocketTitle } from '../components/atoms/PocketTitle';
 import { Header } from '../components/molecules/Header';
-import { MainPocket } from '../components/molecules/MainPocket';
-import { PocketItem } from '../components/molecules/PocketItem';
+import { MainPocket } from '../components/Organisms/MainPocket';
+import { Pockets } from '../components/Organisms/Pockets';
 import { Colors } from '../config/styles';
-import { useAppContext } from '../Contexts/AppContext';
 
-const Home = () => {
-  const { pockets } = useAppContext();
+export const Home = () => {
   return (
     <View style={styles.container}>
       <StatusBar barStyle={'light-content'} backgroundColor={'#8c22d0'} />
       <Header />
-
       <MainPocket />
-      {pockets.map((pocket) => (
-        <PocketItem {...pocket} key={pocket.name}/>
-      ))}
+      <Pockets />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: { flex: 1, backgroundColor: Colors.backGround },
+  container: {
+    flex: 1,
+    backgroundColor: Colors.backGround
+  },
 });
-
-export default Home;
