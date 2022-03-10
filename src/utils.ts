3726ab7dabe866f957f1d9db227a32ad2c5fb1e2
Rafael Moura <rafael.moura22@fatec.sp.gov.br>
2022-03-10 14:50:21 -0300

    iniciando ambiente para usar o asyncstorage

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,24 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { StorageKey } from './enums/StorageKey';
+
+export async function getItem(storageKey: StorageKey) {
+  try {
+    const value = await AsyncStorage.getItem(storageKey);
+    return {success: true, data: JSON.parse(value || '{}')};
+  } catch (error) {
+    return {success: false, error};
+  }
+}
+
+export async function setItem(storageKey: StorageKey, value: any) {
+  try {
+    await AsyncStorage.setItem(storageKey, JSON.stringify(value));
+    return {success: true};
+  } catch (error) {
+    return {success: false, error};
+  }
+}
+
 export function removeDots(text: string) {
   return text.replace(/\./g, '');
 }
