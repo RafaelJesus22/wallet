83830868089c45b4ca0b01cdf6ffc10e19c15e95
Rafael Moura <rafael.moura22@fatec.sp.gov.br>
2022-03-11 16:23:10 -0300

    ajustes na função currencyFormat

--- src/utils.ts
+++ src/utils.ts
@@ -32,5 +32,5 @@ export function currencyFormat(value?: number) {
     return 'R$ 0,00';
   }
 
-  return value.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
+  return `R$ ${value.toFixed(2).replace('.', ',')}`;
 }
