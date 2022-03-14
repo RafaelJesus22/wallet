98f352717521b4895cb0419d38cef16b1b03bae5
Rafael Moura <rafael.moura22@fatec.sp.gov.br>
2022-03-14 09:01:20 -0300

    criando funções do his´torico

--- src/services/History.ts
+++ src/services/History.ts
@@ -1,1 +1,46 @@
-export class HistoryService {}
+import { StorageKey } from "../enums/StorageKey";
+import { HistoryItemProps } from "../types";
+import { getItem, setItem } from "../utils";
+
+export class HistoryService {
+  async getHistory() {
+    const { data } = await getItem(StorageKey.HISTORY);
+
+    if (!data || data === []) {
+      return [];
+    }
+
+    return data;
+  }
+
+  async setHistory(history: HistoryItemProps[]) {
+    const { success, error } = await setItem(StorageKey.HISTORY, history);
+
+    if (!success) {
+      return { success, error };
+    }
+
+    return { success };
+  }
+
+  async getPockethistory(pocketId: string): Promise<HistoryItemProps[]> {
+    const { data } = await getItem(StorageKey.HISTORY);
+
+    if (!data || data === []) {
+      return [];
+    }
+
+    const pocketHistory = data.filter(
+      (history: HistoryItemProps) => history.pocketId === pocketId
+    );
+
+    return pocketHistory;
+  }
+
+  async addPocketHistory(historyItem: HistoryItemProps) {
+    const history = await this.getHistory();
+    const newHistory = [historyItem, ...history];
+
+    return await this.setHistory(newHistory);
+  }
+}
