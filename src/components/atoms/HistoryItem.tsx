e8c97d8ef78e27252b0efd7096777fb09ffd2068
Rafael Moura <rafael.moura22@fatec.sp.gov.br>
2022-03-14 10:31:25 -0300

    adicionando histórico a tela pocketDetails

---
+++ src/components/atoms/HistoryItem.tsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import moment from 'moment';
+
+import { Colors, fontFamily } from '../../config/styles';
+import { HistoryItemProps, TransactionType } from '../../types';
+import { currencyFormat } from '../../utils';
+
+const TODAY = moment().format('DD/MM/YYYY');
+const YESTERDAY = moment().subtract(1, 'days').format('DD/MM/YYYY');
+
+export const HistoryItem: React.FC<HistoryItemProps> = ({
+  date,
+  type,
+  value,
+}) => {
+  const getTitle = (): string => {
+    switch (type) {
+      case "creation":
+        return 'Carteira Criada';
+      case "redeem":
+        return 'Dinheiro Resgatado';
+      case "save":
+        return 'Dinheiro Guardado';
+      default:
+        return '';
+    }
+  };
+
+  const getDate = (): string => {
+    if (moment(date).format('DD/MM/YYYY') === TODAY) {
+      return 'Hoje';
+    }
+    if (moment(date).format('DD/MM/YYYY') === YESTERDAY) {
+      return 'Hoje';
+    }
+
+    return moment(date).format('DD/MM/YYYY');
+  };
+
+  return (
+    <View style={styles.container}>
+      <View>
+        <HistoryIcon type={type} />
+      </View>
+      <View>
+        <Text style={styles.title}>{getTitle()}</Text>
+        <Text style={styles.date}>{getDate()}</Text>
+        <Text style={styles.value}>{currencyFormat(value)}</Text>
+      </View>
+    </View>
+  );
+}
+
+const HistoryIcon: React.FC<{ type: TransactionType }> = ({ type }) => {
+  const backgroundColor = type === 'redeem' ? Colors.placeholder : Colors.successLight;
+  const icons = {
+    save: <Feather name="trending-up" size={36} color={Colors.success} />,
+    redeem: <Feather name="trending-down" size={36} color={Colors.text} />,
+    creation: <Feather name="plus-circle" size={36} color={Colors.success} />,
+  }
+
+  return (
+    <View style={[styles.icon, { backgroundColor }]}>
+      {icons[type]}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderColor: Colors.placeholder,
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+  },
+  icon: {
+    width: 60,
+    height: 60,
+    borderRadius: 60,
+    marginRight: 16,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 18,
+    fontFamily: fontFamily.medium,
+    color: Colors.text,
+  },
+  date: {
+    fontSize: 18,
+    fontFamily: fontFamily.primary,
+    color: Colors.secondatyText,
+  },
+  value: {
+    fontSize: 18,
+    fontFamily: fontFamily.primary,
+    color: Colors.secondatyText,
+  },
+});
--- src/components/molecules/PocketHistory.tsx
+++ src/components/molecules/PocketHistory.tsx
@@ -1,13 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, ActivityIndicator } from 'react-native';
 import { Colors, fontFamily } from '../../config/styles';
+import { useAppContext } from '../../Contexts/AppContext';
+import { HistoryItemProps } from '../../types';
+import { HistoryItem } from '../atoms/HistoryItem';
 
 export const PocketHistory = () => {
   const [loading, setLoading] = useState(false);
+  const [history, setHistory] = useState<HistoryItemProps[]>([]);
+  const { getPocketHistory } = useAppContext();
 
   async function getHistory() {
     setLoading(true);
-    await new Promise(resolve => setTimeout(resolve, 5000));
+    const history = await getPocketHistory();
+    setHistory(history);
     setLoading(false);
   }
 
@@ -25,6 +31,10 @@ export const PocketHistory = () => {
             color={Colors.primary}
           />
         )}
+
+        {history && history.map(h => (
+          <HistoryItem {...h} key={h.id} />
+        ))}
       </View>
     </View>
   );
@@ -32,10 +42,10 @@ export const PocketHistory = () => {
 
 const styles = StyleSheet.create({
   container: {
-    paddingHorizontal: 16,
     paddingTop: 24,
   },
   title: {
+    paddingHorizontal: 16,
     fontSize: 20,
     fontFamily: fontFamily.medium,
     color: Colors.text
